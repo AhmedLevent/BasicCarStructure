0acb4c2cddff5bfac504699ac41f62a117ed2d59
Ahmed Levent Cetinkaya <ahmed.levent.buss@gmail.com>
2023-06-13 12:56:56 +0300

    Basic car structure is created

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        BasicStructure audi = new BasicStructure();
+        audi.model = "A3";
+        audi.color = "Black";
+        audi.power = "5000cc";
+    }
+}
